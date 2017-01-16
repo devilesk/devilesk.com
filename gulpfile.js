2ce7bf7500f1e5f3f5ee639e3bdd166325ec73e2
devilesk <devilesk@gmail.com>
2017-01-16 20:26:38 +0000

    add files to manifest

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('asset-rev', function () {
   return gulp
     .src(['assets/**'])
     .pipe(gulp.dest('content/media'))
-    .pipe(RevAll.revision())
+    .pipe(RevAll.revision({includeFilesInManifest: ['.css', '.js', '.png', '.jpg', '.dmx', '.webm']}))
     .pipe(gulp.dest('content/media'))
     .pipe(RevAll.manifestFile())
     .pipe(gulp.dest('.'));
