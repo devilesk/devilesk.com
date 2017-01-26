e14f5e23fedfec1f291c7b39bf88faf411aea4da
devilesk <devilesk@gmail.com>
2017-01-26 14:33:24 +0000

    update app deployment

--- scripts/app.sh
+++ scripts/app.sh
@@ -1,12 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 # app.sh
-if [ -n "$1" ]; then
-    pushd "apps/$1"
+
+echo $1 $2 $3
+
+if [ -n "$3" ]; then
+    pushd "apps/$3"
+    echo "build... $3"
     npm run build
     popd
-    mkdir -p "$2/$1"
-    cat "apps/$1/src/template/template_data.yaml" "apps/$1/dist/index.j2" > "$2/$1/index.html"
-    rsync -av --exclude='index.*' "apps/$1/dist/" "$2/$1"
-    python scripts/generate_single_file.py $1 --config_file app-prod.yaml
-    echo $1
+    mkdir -p "app_content/$3"
+    mkdir -p "$1/$3"
+    echo "cat... $3"
+    cat "apps/$3/src/template/template_data.yaml" "apps/$3/dist/index.j2" > "app_content/$3/index.html"
+    echo "rsync... $3"
+    rsync -av --exclude='index.*' "apps/$3/dist/" "$1/$3"
+    echo "hyde... $3"
+    python scripts/generate_single_file.py $3 --config_file $2
+    if [ "$2" = "app-prod.yaml" ]; then
+        pushd "apps/$3"
+        echo "rollbar... $3"
+        npm run rollbar
+        popd
+    fi
 fi
